2a3b5e311c1430f5f9511725708751c8689714a8
whit <armstrong.whit@gmail.com>
2008-07-10 13:30:12 -0400

    added getAllChildren

--- R/lim.r
+++ R/lim.r
@@ -2,6 +2,7 @@ get.relation <- function(contract,colnames=NULL,units="days",bars=1) {
   .Call("getRelation", contract, colnames, units, as.integer(bars), PACKAGE="RLIM")
 }
 
-get.contracts <- function(genericContract, units="days",  bars=1) {
-    .Call("getContracts", genericContract, units, as.integer(bars), PACKAGE="RLIM")
+get.all.children <- function(relname) {
+    .Call("getAllChildren", relname)
 }
+
