b9f4ba569958f61825b442b7040541db7d39ac81
whit <armstrong.whit@gmail.com>
2009-07-31 09:49:05 -0400

    updated default roll policy in get.perpetual.series

--- R/lim.r
+++ R/lim.r
@@ -4,7 +4,7 @@ get.relation <- function(relname, colnames=NULL, units="days", bars=1) {
 
 get.perpetual.series <- function(relname, colnames=c("open","high","low","close","volume","OpenInterest"),
                                  rollDay="open_interest crossover",
-                                 rollPolicy="Actual Prices",
+                                 rollPolicy="backward adjusted prices",
                                  units="days",
                                  bars=1) {
 
